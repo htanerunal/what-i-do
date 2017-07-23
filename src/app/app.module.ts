8d8921df64806fd6d5e9a6d0c58010e86bc30802
htanerunal <htaner.unal@gmail.com>
2017-07-23 12:04:43 +0300

    yeni

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDPsLPoWhlbwOJMtSxyvSjfNZRbF8kutCk",
+  authDomain: "angular4project-a4a3e.firebaseapp.com",
+  databaseURL: "https://angular4project-a4a3e.firebaseio.com",
+  projectId: "angular4project-a4a3e",
+  storageBucket: "",
+  messagingSenderId: "1041517030744"
+};
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
